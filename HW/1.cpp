bb9924c3f8407478cf131980279f806b0015eca4
tianxinwen21 <tianxinwen21@outlook.com>
2025-05-01 20:42:07 +0800

    0501

---
+++ HW/1.cpp
@@ -0,0 +1,45 @@
+//1.3
+//#include <iostream>
+//int main()
+//{
+//	std::cout << "Hello, World!" <<std::endl;
+//	return 0;
+//}
+
+//1.4
+//#include <iostream>
+//int main()
+//{
+//	std::cout << "Enter two nums: " <<std::endl;
+//	int v1 = 0, v2 = 0;
+//	std::cin >> v1 >> v2;
+//	std::cout << "The product of " << v1 << " and " << v2
+//	<< " is " << v1 * v2 << std::endl;
+//	return 0;
+//}
+
+//1.5
+//#include <iostream>
+//int main()
+//{
+//	std::cout << "Enter stree nums:" << std::endl;
+//	int v1 = 0, v2 = 0, v3 = 0;
+//	std::cin >> v1 >> v2 >> v3;
+//	std::cout << v1 << std::endl;
+//	std::cout << v2 << std::endl;
+//	std::cout << v3 << std::endl;
+//	return 0;
+//}
+
+//1.6
+//错误，变量在使用前并未定义, 且未正确使用std::cout语句
+//#include <iostream>
+//int main()
+//{
+//	int v1 = 1, v2 = 2;
+//	std::cout << "The sum of " << v1;
+//	std::cout<< " and " << v2;
+//	std::cout<< " is " << v1 + v2 << std::endl;
+//	return 0;
+//}
+//25.05.01
