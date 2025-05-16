ea909030fcc678354cd84a138f8e2f0c40001201
tianxinwen21 <tianxinwen21@outlook.com>
2025-05-16 22:11:31 +0800

    0516

---
+++ HW/第二章/2.32.cpp
@@ -0,0 +1,3 @@
+//int null = 0, *p = null;
+//修改为
+//int null = 0, *p = &null;
---
+++ HW/第二章/2.35.cpp
@@ -0,0 +1,6 @@
+//const int i = 42;
+//i 是一个整型常量 
+//auto j = i; const auto &k = i; auto *p = &i;
+//，j 是整数, k是整型常量， p 是指向整型常量的指针，
+//const auto j2 = i, &k2 = i;
+//j2是整型常量，k2是整型常量的引用
