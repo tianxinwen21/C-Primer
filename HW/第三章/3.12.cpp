2d97607a38311201dce247dc0a2e057ab37a7fab
tianxinwen21 <tianxinwen21@outlook.com>
2025-05-27 21:39:41 +0800

    0527

---
+++ HW/第三章/3.12.cpp
@@ -0,0 +1,5 @@
+//（a）是正确的，定义了一个名为 ivec 的 vector 对象，其中的每个元素都是 vector<int> 对象。
+//
+//（b）是错误的，svec 的元素类型是 string，而 ivec 的元素类型是 int，因此不能使用 ivec 初始化 svec。
+//
+//（c）是正确的，定义了一个名为 svec 的 vector 对象，其中含有 10 个元素，每个元素都是字符串 null。
